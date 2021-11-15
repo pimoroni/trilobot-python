db0701bfade29d7beb483c7b6f9b2c167d577129
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-11-15 14:22:21 +0000

    Added PS4 controller bindings from Brian

--- library/trilobot/controller_mappings.py
+++ library/trilobot/controller_mappings.py
@@ -148,3 +148,34 @@ def create_xbox360_wireless_controller(stick_deadzone_percent=0.2):
     controller.register_trigger_axis("LT", 2, 0, 255, alt_name="L2")
     controller.register_trigger_axis("RT", 5, 0, 255, alt_name="R2")
     return controller
+
+def create_ps4_wireless_controller(stick_deadzone_percent=0.1):
+    controller = SimpleController("Wireless Controller")
+
+    # Button and axis registrations for PS4 Controller
+    controller.register_button("Cross", 304, alt_name="A")
+    controller.register_button("Circle", 305, alt_name="B")
+    controller.register_button("Square", 307, alt_name="X")
+    controller.register_button("Triangle", 308, alt_name="Y")
+    controller.register_button("Start", 315)
+    controller.register_button("Select", 314)
+    controller.register_button("Home", 316)
+    controller.register_button("L1", 310, alt_name="LB")
+    controller.register_axis_as_button("L2", 2, 255, 0, alt_name="LT")
+    controller.register_button("R1", 311, alt_name="RB")
+    controller.register_axis_as_button("R2", 5, 255, 0, alt_name="RT")
+    controller.register_button("Left", 704)
+    controller.register_button("Right", 705)
+    controller.register_button("Up", 706)
+    controller.register_button("Down", 707)
+    controller.register_button("LS", 317)
+    controller.register_button("RS", 318)
+
+    controller.register_axis("LX", 0, 0, 255, deadzone_percent=stick_deadzone_percent)
+    controller.register_axis("LY", 1, 0, 255, deadzone_percent=stick_deadzone_percent)
+    controller.register_axis("RX", 3, 0, 255, deadzone_percent=stick_deadzone_percent)
+    controller.register_axis("RY", 4, 0, 255, deadzone_percent=stick_deadzone_percent)
+    controller.register_trigger_axis("L2", 2, 0, 255, alt_name="LT")
+    controller.register_trigger_axis("R2", 5, 0, 255, alt_name="RT")
+    return controller
+
