37c24da93b0dedbd07b661c9abcdeefbb5b81c0a
Footleg <drfootleg@gmail.com>
2021-11-16 14:33:32 +0000

    Minor fix in print distance example

--- examples/print_distance.py
+++ examples/print_distance.py
@@ -8,14 +8,14 @@ if __name__ == '__main__':
     while True:
 
         #Take 10 measurements rapidly for shorter distances
-        for i in range(100):
+        for i in range(10):
             clockcheck = time.perf_counter()
             distance = tb.read_distance(timeout=25, samples=3)
             print("Rapid:  Distance is {:.1f} cm (took {:.4f} sec)".format(distance,(time.perf_counter() - clockcheck) ) )
             time.sleep(0.01)
 
         #Take 10 measurements slower for longer distances
-        for i in range(100):
+        for i in range(10):
             clockcheck = time.perf_counter()
             distance = tb.read_distance(timeout=200, samples=5)
             print("Slower: Distance is {:.1f} cm (took {:.4f} sec)".format(distance,(time.perf_counter() - clockcheck) ) )
