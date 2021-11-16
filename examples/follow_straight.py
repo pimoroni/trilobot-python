ec95f8b2091df8b250b198add46b429a2a756c2e
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-11-16 19:05:33 +0000

    Added A to stop

--- examples/follow_straight.py
+++ examples/follow_straight.py
@@ -8,7 +8,7 @@ top_speed = 0.7         # The top speed (between 0.0 and 1.0) that the robot wil
 goal_distance = 20.0    # The distance in cm the robot will keep an object in front of it
 speed_range = 5.0       # The distance an object is from the goal that will have the robot drive at full speed
 
-while True:
+while not tb.read_button(tb.BUTTON_A):
     distance = tb.read_distance()
                 
     if distance >= 0.0:
