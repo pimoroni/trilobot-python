4f9fcb75782d9dbd7a072efc02a1ffef8fda5ba2
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-11-19 17:35:04 +0000

    Linting fixes

--- examples/servo_example.py
+++ examples/servo_example.py
@@ -6,14 +6,14 @@ from time import sleep
 
 # if not exists /var/run/pigpio.pid:
 #    stop now
-if (not os.path.isfile('/var/run/pigpio.pid')):
-	print('Please install pigpio with "sudo apt install pigpio"')
-	print('Then run "sudo pigpiod" to enable')
-	sys.exit()
+if not os.path.isfile('/var/run/pigpio.pid'):
+    print('Please install pigpio with "sudo apt install pigpio"')
+    print('Then run "sudo pigpiod" to enable')
+    sys.exit()
 
 from gpiozero.pins.pigpio import PiGPIOFactory
 factory = PiGPIOFactory()
-servo = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
+servo = Servo(12, min_pulse_width=0.5 / 1000, max_pulse_width=2.5 / 1000, pin_factory=factory)
 
 print("Go to middle")
 servo.mid()
@@ -28,7 +28,7 @@ print("And back to middle")
 servo.mid()
 sleep(2)
 print("Servo Off")
-servo.value = None;
+servo.value = None
 
 print("Now Scan")
 while True:
