6ba7309b6830e2318382ea944641a250f4244750
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-02-06 14:59:55 +0000

    Added Xbox One Controller mappings

--- library/trilobot/controller_mappings.py
+++ library/trilobot/controller_mappings.py
@@ -128,13 +128,13 @@ def create_rock_candy_controller():
     return controller
 
 
-def create_xbox360_wireless_controller(stick_deadzone_percent=0.2):
-    """ Create a controller class for the XBox360 Wireless controller.
+def create_xbox_360_wireless_controller(stick_deadzone_percent=0.2):
+    """ Create a controller class for the Xbox 360 Wireless controller.
     stick_deadzone_percent: the deadzone amount to apply to the controller's analog sticks
     """
     controller = SimpleController("Xbox 360 Wireless Receiver")
 
-    # Button and axis registrations for Rock Candy PS3 Controller
+    # Button and axis registrations for Xbox 360 Wireless Controller
     controller.register_button("A", 304, alt_name="Cross")
     controller.register_button("B", 305, alt_name="Circle")
     controller.register_button("X", 307, alt_name="Square")
@@ -159,6 +159,40 @@ def create_xbox360_wireless_controller(stick_deadzone_percent=0.2):
     controller.register_axis("RY", 4, -32768, 32768, deadzone_percent=stick_deadzone_percent)
     controller.register_trigger_axis("LT", 2, 0, 255, alt_name="L2")
     controller.register_trigger_axis("RT", 5, 0, 255, alt_name="R2")
+    return controller
+
+
+def create_xbox_one_wireless_controller(stick_deadzone_percent=0.2):
+    """ Create a controller class for the Xbox One Wireless controller.
+    stick_deadzone_percent: the deadzone amount to apply to the controller's analog sticks
+    """
+    controller = SimpleController("Xbox Wireless Controller")
+
+    # Button and axis registrations for Xbox One Wireless Controller
+    controller.register_button("A", 304, alt_name="Cross")
+    controller.register_button("B", 305, alt_name="Circle")
+    controller.register_button("X", 307, alt_name="Square")
+    controller.register_button("Y", 308, alt_name="Triangle")
+    controller.register_button("Start", 315)
+    controller.register_button("Select", 314)
+    controller.register_button("Home", 316)
+    controller.register_button("LB", 310, alt_name="L1")
+    controller.register_axis_as_button("LT", 3, 255, 0, alt_name="L2")
+    controller.register_button("RB", 311, alt_name="R1")
+    controller.register_axis_as_button("RT", 4, 255, 0, alt_name="R2")
+    controller.register_button("Left", 704)
+    controller.register_button("Right", 705)
+    controller.register_button("Up", 706)
+    controller.register_button("Down", 707)
+    controller.register_button("LS", 317, alt_name="L3")
+    controller.register_button("RS", 318, alt_name="R3")
+
+    controller.register_axis("LX", 0, 0, 65535, deadzone_percent=stick_deadzone_percent)
+    controller.register_axis("LY", 1, 0, 65535, deadzone_percent=stick_deadzone_percent)
+    controller.register_axis("RX", 2, 0, 65535, deadzone_percent=stick_deadzone_percent)
+    controller.register_axis("RY", 5, 0, 65535, deadzone_percent=stick_deadzone_percent)
+    controller.register_trigger_axis("LT", 3, 0, 255, alt_name="L2")
+    controller.register_trigger_axis("RT", 4, 0, 255, alt_name="R2")
     return controller
 
 
@@ -241,7 +275,8 @@ def choose_controller():
                        ("8Bitdo SN30", create_8bitdo_sn30_controller),
                        ("8BitDo SN30 Pro+", create_8bitdo_sn30_pro_controller),
                        ("RockCandy Controller", create_rock_candy_controller),
-                       ("XBox360 Wireless Receiver", create_xbox360_wireless_controller),
+                       ("Xbox 360 Wireless Receiver", create_xbox_360_wireless_controller),
+                       ("Xbox One Wireless Receiver", create_xbox_one_wireless_controller),
                        ("PS4 Controller", create_ps4_wireless_controller),
                        ("PS4 Controller - Touchpad", create_ps4_wireless_controller_touchpad),
                        ("PS4 Controller - Motion Sensors", create_ps4_wireless_controller_motion)]
